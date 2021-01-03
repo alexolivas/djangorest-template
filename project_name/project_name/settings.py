2a29e1d2965c43300d9f862250657641f40f0180
Alex Olivas <alex.olivas@gmail.com>
2021-01-03 14:08:17 -0500

    added environment variables for debugging

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'gi77%%8&15=*$qkv(ijj8bi&&j01h%$82*)zeybcd)9nauw(7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 ALLOWED_HOSTS = []
 
